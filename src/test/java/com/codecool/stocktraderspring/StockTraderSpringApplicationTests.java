65f36d166a6c8c7f2652285913a10edf66f7afb5
Vivien Walter <walter.vivien.k@gmail.com>
2020-06-21 12:09:15 +0200

    first commit

---
+++ src/test/java/com/codecool/stocktraderspring/StockTraderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktraderspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTraderSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
